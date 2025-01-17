3d7fd0e137c967ac731aa0c1c7610b7e79bdf714
unknown <romakiri2011@gmail.com>
2025-01-17 21:38:54 +0200

    Method of class OOP

--- gggg.py
+++ gggg.py
@@ -1,22 +1,28 @@
-class Student:
-    def __init__(self, name=None, height=190):
+class people:
+    def __init__(self, name=None, height=156, speed=23):
         self.name = name
         self.height = height
+        self.speed = speed
 
     def __str__(self):
-        return f'I am a student and my name is {self.name}'
+        return f'I am a people and my name is {self.name}, and height {self.height}, my speed i {self.speed}'
 
-    def __bool__(self):
-        return self.name != None
-
-    def __len__(self):
-        return self.height
-
-    def __del__(self):
-        print("I am deleted")
+people = people(name="Roma")
+print(people.__str__())
 
 
-first_student = Student(name="Nick")
-print(first_student.__len__())
-print(first_student.__bool__())
-print(first_student)
+class dog:
+    def __init__(self, name="luna", weight=7, color="black", speed=36):
+        self.name = name
+        self.weight = weight
+        self.color = color
+        self.speed = speed
+
+dog = dog(weight=7, name="Luna", color="black", speed=36)
+print(f"dog weight is {dog.weight}, and name {dog.name}, color is {dog.color}, and move speed {dog.speed}")
+
+
+
+
+
+
