10334cb79025b9d21e23577dcc1dcb079cadfe0e
Pirat656 <romakiri2011@gmail.com>
2025-01-24 20:02:06 +0200

    gggg.py

--- gggg.py
+++ gggg.py
@@ -1,27 +1,73 @@
-class people:
-    def __init__(self, name=None, height=156, speed=23):
+import random
+
+
+class Student:
+
+    def __init__(self, name, ):
         self.name = name
-        self.height = height
-        self.speed = speed
+        self.money = 0
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
 
-    def __str__(self):
-        return f'I am a people and my name is {self.name}, and height {self.height}, my speed i {self.speed}'
+    def to_work(self):
+        print("!TIME TO WORK!")
+        self.gladness += 1.25
 
-people = people(name="Roma")
-print(people.__str__())
+    def to_study(self):
+        print("Time to study")
+        self.progress += 0.12
+        self.gladness -= 3
+
+    def to_sleep(self):
+        print("Time to sleep")
+        self.gladness += 3
+
+    def to_chill(self):
+        print("Time to rest")
+        self.gladness += 5
+        self.progress -= 0.1
 
 
-class dog:
-    def __init__(self, name="luna", weight=7, color="black", speed=36):
-        self.name = name
-        self.weight = weight
-        self.color = color
-        self.speed = speed
+    def is_alive(self):
+        if self.progress < 0.5:
+            print("Start hard working")
+            self.alive = True
+        elif self.gladness <= 0:
+            print("Go walking")
+            self.alive = True
+        elif self.progress > 5:
+            print("Study more")
+            self.alive = True
+        elif self.money < 1:
+            print("!TIME TO WORK!")
+            self.to_work()
 
-dog = dog(weight=7, name="Luna", color="black", speed=36)
-print(f"dog weight is {dog.weight}, and name {dog.name}, color is {dog.color}, and move speed {dog.speed}")
+    def end_of_day(self):
+        print(f"Gladness - {self.gladness}")
+        print(f"Progress - {round(self.progress, 2)}")
+
+    def life(self, day):
+        day = "Day " + str(day + 1) + " of " + self.name + " life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+        if live_cube == 1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_work()
+        elif live_cube == 3:
+            self.to_chill()
+        self.to_sleep()
+        self.end_of_day()
+        self.is_alive()
 
 
+nick = Student(name="Nick")
+
+for day in range(365):
+    if nick.alive == False:
+        break
+    nick.life(day)
 
 
 
